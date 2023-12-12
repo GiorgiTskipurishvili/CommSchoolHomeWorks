6fce8e25a38c1c4d0c43046caf65bbde9da769e1
GiorgiTskipurishvili <giorgitskipurishvili@gmail.com>
2023-12-12 21:14:46 +0400

    Homework 2

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Giorgi Tskipurishvili");
+
+            Console.Write("Enter Something: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Output: " + userInput);
+
+            Console.ReadKey();
+        }
+    }
+}
